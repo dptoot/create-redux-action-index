dfb5c148471a094fce9ef39dba0ecc58421c23aa
James Lewis <james.lewis@autotrader.com>
2017-06-01 16:28:31 -0400

    chore: add initial test dirs

---
+++ test/actions2/index.js
@@ -0,0 +1,9 @@
+import * as testFile1Actions from './testFile1.js';
+import * as testFile2Actions from './testFile2.js';
+import * as testFile3Actions from './testFile3.js';
+
+export default Object.assign({}, 
+    testFile1Actions,
+    testFile2Actions,
+    testFile3Actions
+);
